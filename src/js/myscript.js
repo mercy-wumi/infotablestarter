7b3faa7455b36fd1c1fc9e1b3707b8626e10da24
mercy-wumi <m.adams1909@gmail.com>
2021-12-04 23:53:13 +0100

    completed challenge

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,47 +1,39 @@
 console.log('Welcome to HackerU-JustCode Assignment');
 $(document).ready(function () {
-    getRemoteData();
+  getRemoteData();
 });
 function getRemoteData() {
-    // write your code here to fetch data from api. 
-    fetch('https://www.justcode.com/dataservice/api/paymentMethod/list')
-        .then(response => response.json())
-        .then(responseJson =>
-            displayResults(responseJson))
-        .catch(err => {
-            console.log(err)
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
+  // write your code here to fetch data from api. 
+  fetch(`https://www.justcode.com/dataservice/api/paymentMethod/list`)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
 }
 
-
 function displayResults(responseJson) {
-    console.log(responseJson);
-    //reads the data from the fetch and creates html
-    $("#mytable").html("")//clears the results
-    responseJson.forEach(table => {//loops through each data in the array
-        $("#mytable").append(`
-      <tr>
-      <th>${table.id}</th>
-      <th>${table.methodName}</th>
-      <th>${table.medium}</th>
-      <th>${table.yearOfOrigin}</th>
-      <th>${table.modifiedOn}</th>
-      </tr>
-      `)//appends table to results div
-    })
+  console.log(responseJson);
+  $('#mytable').html("")
+  responseJson.forEach(table => {
+    $('#mytable').append(`
+        <tr>
+        <th>${table.id}</th>
+        <th>${table.methodName}</th>
+        <th>${table.medium}</th>
+        <th>${table.yearOfOrigin}</th>
+        <th>${table.modifiedOn}</th>
+        </tr>
+        `)
+  })
 }
 
 
 function watchForm() {
-    $('#mytable').submit(event => {
-    //clears the results
-        event.preventDefault();
-        getRemoteData();
-    });
+  $('#mytable').submit(e => {
+    e.preventDefault();
+    getRemoteData();
+  })
 }
 //run this function 1st anytime the app starts or the document is ready
 $(function () {
-    console.log('App loaded! Waiting!');
-    watchForm();
+  console.log('App loaded! Waiting!');
+  watchForm();
 });
